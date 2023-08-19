70c3e5f047dfc8b885a67c3a93fbf2f02fda56cd
aaarajuan <aaa.rajuan@gmail.com>
2023-08-19 19:32:36 +0600

    added logics to balance total in js

--- js/deposit.js
+++ js/deposit.js
@@ -13,8 +13,21 @@ document.getElementById("btn-deposit").addEventListener("click", function(){
     const depositTotalElement = document.getElementById("deposit-total");
     const previousDepositTotalString = depositTotalElement.innerText;
     const previousDepositTotal = parseFloat(previousDepositTotalString);
+    
+    // step 4: add numbers to set the total deposit
     const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    // set the deposit total
     depositTotalElement.innerText = currentDepositTotal;
+
+    // step 5: get balance current total
+    const currentBalanceElement = document.getElementById("balance-total");
+    const currentBalanceString = currentBalanceElement.innerText;
+    const currentBalance = parseFloat(currentBalanceString);
+
+    // step 6: calculate current total banalce
+    const currentBalanceTotal = previousBalance + newDepositAmount;
+    // set the balance total
+    currentBalanceElement.innerText = currentBalanceTotal;
 
     // step 7: clear the deposit field
     depositField.value = '';
