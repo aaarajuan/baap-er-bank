8f87dcd315fff5cb2f5c0da7d608a30b3e9b181d
aaarajuan <aaa.rajuan@gmail.com>
2023-08-19 17:28:23 +0600

    created login form on baap er bank

---
+++ js/login.js
@@ -0,0 +1,22 @@
+// step 1: add click event handler with the submit button
+document.getElementById('btn-submit').addEventListener('click', function () {
+    // step 2: get email address inside the input field
+    // always remember to use .value to get text from an input field
+    const emailFiels = document.getElementById('user-email');
+    const email = emailFiels.value;
+    // step 3: get password
+    // 3.a: set id on the html element
+    // 3.b: get the element
+    // 3.c: get the value from the element
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+
+    // DANGER: don not verify email passwrod on client side
+    // step 4: verify email and password
+    if (email === 'ongkur@gmail.com' && password === 'secret') {
+        console.log('valid user');
+    }
+    else {
+        console.log('invalid user');
+    }
+})
